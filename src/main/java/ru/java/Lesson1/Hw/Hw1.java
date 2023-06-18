f68423cdc1767ce09fb8aa974a765ee89813c81e
Sergei <honholoi.serg09@mail.ru>
2023-06-18 14:37:01 +0900

    hw2

--- src/main/java/ru/java/Lesson1/Hw/Hw1.java
+++ src/main/java/ru/java/Lesson1/Hw/Hw1.java
@@ -2,10 +2,10 @@ package ru.java.Lesson1.Hw;
 
 public class Hw1 {
     class Solution {
-    public String reverseWords(String s) {
-        List<String> words = Arrays.asList(s.trim().split("\\s+"));
-        Collections.reverse(words);
-        return String.join(" ", words);
+        public String reverseWords(String s) {
+            List<String> words = Arrays.asList(s.trim().split("\\s+"));
+            Collections.reverse(words);
+            return String.join(" ", words);
+        }
     }
-}
 }
