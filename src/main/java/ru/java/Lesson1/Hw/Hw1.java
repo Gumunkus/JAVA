439d850659d032ece814a1b01af7973668bd453b
Sergei <honholoi.serg09@mail.ru>
2023-06-18 14:19:21 +0900

    hw

---
+++ src/main/java/ru/java/Lesson1/Hw/Hw1.java
@@ -0,0 +1,11 @@
+package ru.java.Lesson1.Hw;
+
+public class Hw1 {
+    class Solution {
+    public String reverseWords(String s) {
+        List<String> words = Arrays.asList(s.trim().split("\\s+"));
+        Collections.reverse(words);
+        return String.join(" ", words);
+    }
+}
+}
